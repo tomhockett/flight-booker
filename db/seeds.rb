5e93fb76351814fb17bc1cc6d17bbd5235509ceb
Tom Hockett <tom.hockett@gmail.com>
2021-07-28 22:35:54 -0500

    seed database with 12 flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,18 @@ require 'CSV'
 
 ActiveRecord::Base.transaction do
   Airport.destroy_all
-  # Flight.destroy_all
+  Flight.destroy_all
 
   airport_file_path = File.join Rails.root, 'us-airports.csv'
   CSV.foreach(airport_file_path,
               headers: :first_row,
               header_converters: :symbol) do |row|
     Airport.create(name: row[:name], airport_code: row[:iata_code]) if row[:iata_code].present?
+  end
+
+  santa_ana = Airport.find_by airport_code: 'SNA'
+  la_guardia = Airport.find_by airport_code: 'LGA'
+  (1..12).each do |hour|
+    Flight.create(departure_id: santa_ana.id, destination_id: la_guardia.id, takeoff: Time.zone.now + hour.hour, duration: 24000)
   end
 end
