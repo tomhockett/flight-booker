ed03f7e2fe3f368d6a9a4aeacbf742a868aac123
Tom Hockett <tom.hockett@gmail.com>
2021-08-13 00:56:14 -0500

    Trim off Airport cause it's redundant

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,8 @@
 class Airport < ApplicationRecord
   has_many :departing_flights, class_name: 'Flight', foreign_key: :departure_id, dependent: :destroy
   has_many :arriving_flights, class_name: 'Flight', foreign_key: :destination_id, dependent: :destroy
+
+  def trimmed_name_to_s
+    name.gsub(' Airport', '')
+  end
 end
