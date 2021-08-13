0a765252a986012d0bc8ca3c3b111bfa009d5127
Tom Hockett <tom.hockett@gmail.com>
2021-08-13 00:55:53 -0500

    Add flight description for display

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, class_name: 'Airport', foreign_key: 'departure_id'
   belongs_to :to_airport, class_name: 'Airport', foreign_key: 'destination_id'
+
+  def description
+    "Flight ##{id} from #{from_airport.trimmed_name_to_s} to #{to_airport.trimmed_name_to_s}."
+  end
 end
